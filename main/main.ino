2ae44b6c8f372f291c0008da50069b239f0114bd
igni <S00286089@atu.ie>
2026-03-04 19:27:07 +0000

    Update main.ino

--- main/main.ino
+++ main/main.ino
@@ -1,14 +1,15 @@
 #include <Arduino.h>
 #include <U8g2lib.h>
+#include <PulseSensorPlayground.h>  // Includes the PulseSensorPlayground Library.
 
-//tf is all this
+//This code just checks whether we are using SPI or I2C, we can remove most of this and just include "Wire.h" because we will only use I2C
 #ifdef U8X8_HAVE_HW_SPI
 #include <SPI.h>
 #endif
 #ifdef U8X8_HAVE_HW_I2C
 #include <Wire.h>
 #endif
-#include <PulseSensorPlayground.h>  // Includes the PulseSensorPlayground Library.
+
 
 //  Variables
 U8G2_SSD1306_128X64_NONAME_F_HW_I2C u8g2(U8G2_R0, /* reset=*/ U8X8_PIN_NONE);
