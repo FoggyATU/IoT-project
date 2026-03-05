09f3d6186c66fa3dd608178cb95cb2390bc66498
Zoriana Nychkalo <s00289335@atu.ie>
2026-03-05 17:02:46 +0000

    Removed unnecessary code

--- main/main.ino
+++ main/main.ino
@@ -1,12 +1,8 @@
 #include <Arduino.h>
 #include <U8g2lib.h>
 
-#ifdef U8X8_HAVE_HW_SPI
-#include <SPI.h>
-#endif
-#ifdef U8X8_HAVE_HW_I2C
+
 #include <Wire.h>
-#endif
 #include <PulseSensorPlayground.h>
 
 U8G2_SSD1306_128X64_NONAME_F_HW_I2C u8g2(U8G2_R0, /* reset=*/ U8X8_PIN_NONE); //Setup u8g2
