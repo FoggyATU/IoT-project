9f3c559842c0e4d077c50f63033eb9d38a3e1a6c
FoggyATU <S00286413@atu.ie>
2026-04-20 16:07:22 +0100

    Simple stuff

--- phoneapp/main.gd
+++ phoneapp/main.gd
@@ -1,10 +1,12 @@
 extends Control
 
-const URL:String = "https://cool.foggydude.dev/?url={0}"
+const REFRESH_URL:String = "http://127.0.0.1:6768"
+const DOWNLOAD_URL:String = "https://cool.foggydude.dev/?url={0}"
 
-@onready var http:AwaitableHTTPRequest = $Http
+@onready var download:AwaitableHTTPRequest = $Downloader
 @onready var player:AudioStreamPlayer = $Player
 @onready var info_label:Label = $InfoLabel
+@onready var refresh_timer: Timer = $RefreshTimer
 
 #TODO LIST:
 #Do a request every like 2secs to the Arduino to see what song to play
@@ -12,12 +14,24 @@ const URL:String = "https://cool.foggydude.dev/?url={0}"
 #Cool awesome interface
 
 func _ready():
-	info_label.text = "Loading..."
-	await download_audio("ranweBZaoTQ")
-	info_label.text = "Playing! i dont know the name though"
+	pass
+	#info_label.text = "Loading..."
+	#await download_audio("ranweBZaoTQ")
+	#info_label.text = "Playing! i dont know the name though"
+
+func get_audio_data():
+	print("trying to get data")
+	var resp := await download.async_request(REFRESH_URL)
+	if !resp.success() or resp.status_err():
+		push_error("Request failed.")
 	
+	print("Success")
+	var output = resp.body_as_string()
+	print(resp.bytes)
+	refresh_timer.start()
+
 func download_audio(video_url:String):
-	var resp := await http.async_request(URL.format([video_url]))
+	var resp := await download.async_request(DOWNLOAD_URL.format([video_url]))
 	if !resp.success() or resp.status_err():
 		push_error("Request failed.")
 
